5386ce072803ba4d7a660902df4c333fcfd718d2
nthanhtai-005 <nthanhtaii205@gmail.com>
2026-06-05 12:17:47 +0700

    Thêm ExecuteQuery và ExecuteScalar

--- DAL/Repositories/DbExecutor.cs
+++ DAL/Repositories/DbExecutor.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace DAL.Repositories
 {
@@ -25,6 +26,36 @@ namespace DAL.Repositories
                 using (OracleCommand cmd = new OracleCommand(sqlCommand, conn))
                 {
                     cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        // Hàm TRẢ VỀ DỮ LIỆU DẠNG BẢNG (Dùng cho SELECT lấy danh sách, đổ lên DataGridView)
+        public DataTable ExecuteQuery(string sqlCommand)
+        {
+            DataTable dataTable = new DataTable();
+            using (OracleConnection conn = _connectionManager.GetConnection())
+            {
+                conn.Open();
+                using (OracleCommand cmd = new OracleCommand(sqlCommand, conn))
+                {
+                    using (OracleDataAdapter adapter = new OracleDataAdapter(cmd))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            return dataTable;
+        }
+
+        // 3. Hàm TRẢ VỀ 1 GIÁ TRỊ DUY NHẤT (Dùng cho SELECT lấy 1 ô dữ liệu, ví dụ lấy mã Hash, hoặc COUNT)
+        public object ExecuteScalar(string sqlCommand)
+        {
+            using (OracleConnection conn = _connectionManager.GetConnection())
+            {
+                conn.Open();
+                using (OracleCommand cmd = new OracleCommand(sqlCommand, conn))
+                {
+                    return cmd.ExecuteScalar();
                 }
             }
         }
