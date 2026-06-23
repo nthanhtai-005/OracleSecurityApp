fdaa8f5aecafbcc887b4cc04125f808091105b1d
nthanhtai-005 <nthanhtaii205@gmail.com>
2026-06-23 18:21:08 +0700

    update

--- DTO/RoleModel.cs
+++ DTO/RoleModel.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BLL.Models
+namespace DTO
 {
-    internal class PrivilegeModel
+    internal class RoleModel
     {
     }
 }
